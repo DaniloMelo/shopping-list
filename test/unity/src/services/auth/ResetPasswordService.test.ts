65f7008e22ebd27b7e418c5b3a79e41cc6ed72f8
Danilo Melo <danilomarquesdemelo@gmail.com>
2025-02-25 13:53:08 -0300

    feat: add ResetPasswordService with requestResetPassword method and ResetPasswordService.test.ts

---
+++ test/unity/src/services/auth/ResetPasswordService.test.ts
@@ -0,0 +1,118 @@
+import { ResetPasswordServiceError } from "@/lib/CustomErrors";
+import { IHasher } from "@/lib/Hasher";
+import { IMailer } from "@/lib/Mailer";
+import { ITokenService } from "@/lib/TokenService";
+import { IResetPasswordRepository } from "@/repository/ResetPasswordRepository";
+import { IUserRepository } from "@/repository/UserRepository";
+import ResetPasswordService from "@/services/auth/ResetPasswordService";
+
+const mockUserRepository: jest.Mocked<IUserRepository> = {
+  createUser: jest.fn(),
+  findUserByEmail: jest.fn(),
+  findUserById: jest.fn(),
+  updatePassword: jest.fn(),
+};
+
+const mockResetPasswordRepository: jest.Mocked<IResetPasswordRepository> = {
+  createResetPasswordToken: jest.fn(),
+  findResetPasswordToken: jest.fn(),
+  deleteAllResetPasswordTokens: jest.fn(),
+};
+
+const mockTokenService: jest.Mocked<ITokenService> = {
+  generate: jest.fn(),
+  verify: jest.fn(),
+};
+
+const mockHasher: jest.Mocked<IHasher> = {
+  encrypt: jest.fn(),
+  decrypt: jest.fn(),
+};
+
+const mockMailer: jest.Mocked<IMailer> = {
+  send: jest.fn(),
+};
+
+describe("src/services/auth/ResetPasswordService.ts", () => {
+  let resetPasswordService: ResetPasswordService;
+
+  beforeEach(() => {
+    jest.clearAllMocks();
+    resetPasswordService = new ResetPasswordService(
+      mockUserRepository,
+      mockResetPasswordRepository,
+      mockTokenService,
+      mockHasher,
+      mockMailer,
+    );
+  });
+
+  describe("requestResetPassword method", () => {
+    describe("Successfull Cases", () => {
+      test("Should call requestResetPassword method and send an email", async () => {
+        const dbUser = {
+          id: "123",
+          name: "John Doe",
+          email: "john@email.com",
+          password: "hashed-password",
+          createdAt: new Date(),
+        };
+
+        mockUserRepository.findUserByEmail.mockResolvedValue(dbUser);
+        mockResetPasswordRepository.findResetPasswordToken.mockResolvedValue(null);
+        mockTokenService.generate.mockResolvedValue("fake-test-token");
+        mockResetPasswordRepository.createResetPasswordToken.mockResolvedValue(undefined);
+        mockMailer.send.mockResolvedValue(undefined);
+
+        await expect(resetPasswordService.requestResetPassword("john@email.com")).resolves.toBeUndefined();
+      });
+    });
+
+    describe("Failure Cases", () => {
+      test("Shold throw an error if user don't exist", async () => {
+        mockUserRepository.findUserByEmail.mockResolvedValue(null);
+
+        await expect(resetPasswordService.requestResetPassword("john@email.com")).rejects.toThrow(
+          ResetPasswordServiceError,
+        );
+      });
+
+      test("Shold throw an error if some reset password token already exists", async () => {
+        const dbUser = {
+          id: "123",
+          name: "John Doe",
+          email: "john@email.com",
+          password: "hashed-password",
+          createdAt: new Date(),
+        };
+
+        const dbResetPasswordToken = {
+          id: "456",
+          token: "fake-test-token",
+          userId: "123",
+          expiresAt: new Date(Date.now() + 10 * 60 * 1000),
+          createdAt: new Date(),
+        };
+
+        mockUserRepository.findUserByEmail.mockResolvedValue(dbUser);
+        mockResetPasswordRepository.findResetPasswordToken.mockResolvedValue(dbResetPasswordToken);
+
+        await expect(resetPasswordService.requestResetPassword("john@email.com")).rejects.toThrow(
+          ResetPasswordServiceError,
+        );
+      });
+    });
+  });
+
+  // describe("executeResetPassword method", () => {
+
+  // })
+});
+
+// const dbResetPasswordToken = {
+//   id: "456",
+//   token: "fake-test-token",
+//   userId: "123",
+//   expiresAt: new Date(Date.now() + 10 * 60 * 1000),
+//   createdAt: new Date(),
+// };
