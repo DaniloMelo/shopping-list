005b019122bc5ed93091d6dd51a3ce2463aa4dd6
Danilo Melo <danilomarquesdemelo@gmail.com>
2025-03-04 16:09:40 -0300

    fix: remove comments in middleware.ts

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,21 +1,23 @@
 import { NextRequest, NextResponse } from "next/server";
-// import TokenService from "./lib/TokenService";
+import TokenService from "./lib/TokenService";
 
-// const tokenService = new TokenService();
+const tokenService = new TokenService();
 
 export async function middleware(req: NextRequest) {
-  // const sessionToken = req.cookies.get("sessionToken")?.value;
-  // console.log("Token no middleware: ", sessionToken);
-  // if (!sessionToken) {
-  //   console.log("Redirecionando para login: Token não encontrado");
-  //   return NextResponse.redirect(new URL("/login", req.url));
-  // }
+  const sessionToken = req.cookies.get("sessionToken")?.value;
+  console.log("Token no middleware: ", sessionToken);
+  if (!sessionToken) {
+    console.log("Redirecionando para login: Token não encontrado");
+    return NextResponse.redirect(new URL("/login", req.url));
+  }
 
   try {
-    // await tokenService.verify(sessionToken);
-    // const response = NextResponse.next();
-    // response.headers.set("x-user-data", sessionToken);
-    // return response;
+    await tokenService.verify(sessionToken);
+
+    const response = NextResponse.next();
+    response.headers.set("x-user-data", sessionToken);
+
+    return response;
   } catch (error) {
     console.error("Erro ao verificar token no middleware: ", error);
     return NextResponse.redirect(new URL("/login", req.url));
