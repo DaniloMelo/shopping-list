d31d355880a06e74f19b5fc6a761208290ca39e5
Danilo Melo <danilomarquesdemelo@gmail.com>
2025-03-13 23:15:25 -0300

    feat: add listProducts method in ShoppingListService.ts and tests

--- test/unity/src/services/ShoppingListService.test.ts
+++ test/unity/src/services/ShoppingListService.test.ts
@@ -11,6 +11,7 @@ const mockUserRepository: jest.Mocked<IUserRepository> = {
 
 const mockShoppingListRepository: jest.Mocked<IShoppingListRepository> = {
   create: jest.fn(),
+  findAll: jest.fn(),
 };
 
 describe("src/services/ShoppingListService.ts", () => {
@@ -82,7 +83,7 @@ describe("src/services/ShoppingListService.ts", () => {
         await expect(shoppingListService.createProduct(newProduct)).rejects.toThrow("Quantidade do produto inválida.");
       });
 
-      test("Shpuld throw InternalServerError if a unexpected error occurs", async () => {
+      test("Should throw InternalServerError if a unexpected error occurs", async () => {
         const newProduct = {
           productName: "Mouse",
           productPrice: 199.99,
@@ -103,6 +104,59 @@ describe("src/services/ShoppingListService.ts", () => {
 
         await expect(shoppingListService.createProduct(newProduct)).rejects.toThrow(
           "Ocorreu um erro inesperado ao tentar adicionar um novo produto.",
+        );
+      });
+    });
+  });
+
+  describe("listProducts method: ", () => {
+    describe("Successfull Cases", () => {
+      test("Should find and return all products", async () => {
+        const dbProducts = [
+          {
+            id: "098zxc",
+            createdAt: new Date(),
+            userId: "123abc",
+            productName: "Mouse",
+            productPrice: 199.99,
+            productQuantity: 1,
+            updatedAt: new Date(),
+          },
+        ];
+
+        (mockShoppingListRepository.findAll as jest.Mock).mockResolvedValue(dbProducts);
+
+        const result = await shoppingListService.listProducts("123abc");
+
+        expect(mockShoppingListRepository.findAll).toHaveBeenCalledWith("123abc");
+        expect(result).toEqual(dbProducts);
+      });
+
+      test("Should return an empty array if the user has no products", async () => {
+        (mockShoppingListRepository.findAll as jest.Mock).mockResolvedValue([]);
+
+        const result = await shoppingListService.listProducts("123abc");
+
+        expect(mockShoppingListRepository.findAll).toHaveBeenCalledWith("123abc");
+        expect(result).toEqual([]);
+      });
+    });
+
+    describe("Failure Cases", () => {
+      test("Should throw InternalServerError if a unexpected error occurs", async () => {
+        const user = {
+          id: "123456",
+          name: "John Doe",
+          email: "john@email.com",
+          password: "P4ssword!23",
+          createdAt: new Date(),
+        };
+
+        (mockUserRepository.findUserById as jest.Mock).mockResolvedValue(user);
+        (mockShoppingListRepository.findAll as jest.Mock).mockRejectedValue(new Error("Unexpected error."));
+
+        await expect(shoppingListService.listProducts("123abc")).rejects.toThrow(
+          "Ocorreu um erro inesperado ao tentar buscar a lista de produtos.",
         );
       });
     });
