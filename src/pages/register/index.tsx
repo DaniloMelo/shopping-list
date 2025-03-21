9d55e2b5198014021b4c91c19ce7e81c85590aca
Danilo Melo <danilomarquesdemelo@gmail.com>
2025-03-21 19:01:25 -0300

    refactor: add /auth directory and move /register and /login to /auth

--- src/pages/register/index.tsx
+++ src/pages/register/index.tsx
@@ -47,7 +47,7 @@ export default function RegisterPage() {
     setIsLoading(true);
 
     try {
-      const response = await fetch("api/v1/register", {
+      const response = await fetch("api/v1/auth/register", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(newUser),
