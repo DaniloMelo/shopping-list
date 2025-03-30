f9770c8ae94871daae36f7534e584c2cc5a7b931
Danilo Melo <danilomarquesdemelo@gmail.com>
2025-03-30 20:43:22 -0300

    feat: add fullUpdate and partialUpdate methods in ShoppingListRepository.ts

--- src/repository/ShoppingListRepository.ts
+++ src/repository/ShoppingListRepository.ts
@@ -11,9 +11,17 @@ export interface IDbProduct {
   updatedAt: Date;
 }
 
+export interface IUpdateProduct {
+  productName: string;
+  productPrice: number;
+  productQuantity: number;
+}
+
 export interface IShoppingListRepository {
   create(newProductObj: INewProduct): Promise<void>;
   findAll(userId: string): Promise<IDbProduct[] | []>;
+  fullUpdate(productId: string, product: IUpdateProduct): Promise<void>;
+  partialUpdate(productId: string, product: Partial<INewProduct>): Promise<void>;
 }
 
 export default class ShoppingListRepository implements IShoppingListRepository {
@@ -26,6 +34,20 @@ export default class ShoppingListRepository implements IShoppingListRepository {
   async findAll(userId: string): Promise<IDbProduct[] | []> {
     return await prisma.shoppingList.findMany({
       where: { userId },
+    });
+  }
+
+  async fullUpdate(prodId: string, product: IUpdateProduct): Promise<void> {
+    await prisma.shoppingList.update({
+      where: { id: prodId },
+      data: product,
+    });
+  }
+
+  async partialUpdate(productId: string, PartialProduct: Partial<INewProduct>): Promise<void> {
+    await prisma.shoppingList.update({
+      where: { id: productId },
+      data: PartialProduct,
     });
   }
 }
