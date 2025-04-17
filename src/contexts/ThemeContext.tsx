6d79e311b3ccee872dcc2a5391d54cff78f278db
Danilo Melo <danilomarquesdemelo@gmail.com>
2025-04-17 13:30:08 -0300

    feat: add ThemeContext.tsx and useTheme.ts

---
+++ src/contexts/ThemeContext.tsx
@@ -0,0 +1,31 @@
+import { createContext, ReactNode, useEffect, useState } from "react";
+
+type Theme = "dark" | "light";
+
+interface ThemeContextProps {
+  theme: Theme;
+  toggleTheme(): void;
+}
+
+interface ThemeProviderProps {
+  children: ReactNode;
+}
+
+const ThemeContext = createContext<ThemeContextProps | undefined>(undefined);
+
+export function ThemeProvider({ children }: ThemeProviderProps) {
+  const [theme, setTheme] = useState<Theme>("light");
+
+  useEffect(() => {
+    document.body.classList.remove("light", "dark");
+    document.body.classList.add(theme);
+  });
+
+  function toggleTheme() {
+    setTheme((prev) => (prev === "light" ? "dark" : "light"));
+  }
+
+  return <ThemeContext.Provider value={{ theme, toggleTheme }}>{children}</ThemeContext.Provider>;
+}
+
+export default ThemeContext;
---
+++ src/hooks/useTheme.ts
@@ -0,0 +1,12 @@
+import ThemeContext from "@/contexts/ThemeContext";
+import { useContext } from "react";
+
+export default function useTheme() {
+  const context = useContext(ThemeContext);
+
+  if (!context) {
+    throw new Error("useTheme must be used within a ThemeProvider.");
+  }
+
+  return context;
+}
