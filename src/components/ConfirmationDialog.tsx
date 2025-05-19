1c90308ca2289c60138465a8386dc1854cc4d0c2
Danilo Melo <danilomarquesdemelo@gmail.com>
2025-05-19 13:48:12 -0300

    fix: add outsite click to close ConfirmationDialog.tsx

--- src/components/ConfirmationDialog.tsx
+++ src/components/ConfirmationDialog.tsx
@@ -8,8 +8,12 @@ export default function ConfirmationDialog({ message, isOpen, onConfirmation }: 
   return (
     <section
       className={`flex justify-center items-center fixed h-screen w-screen top-0 left-0 bg-zinc-700/50 backdrop-blur-sm z-10 ${isOpen ? "fixed" : "hidden"}`}
+      onClick={() => onConfirmation(false)}
     >
-      <div className="flex flex-col items-center flex-1 max-w-4xl p-8 mx-8 rounded-lg bg-primaryLightBG dark:bg-primaryDarkBG ">
+      <div
+        className="flex flex-col items-center flex-1 max-w-4xl p-8 mx-8 rounded-lg bg-primaryLightBG dark:bg-primaryDarkBG"
+        onClick={(e) => e.stopPropagation()}
+      >
         <p className="mb-6 text-lightTxt dark:text-darkTxt">{message}</p>
 
         <div className="flex justify-center items-center gap-x-4">
